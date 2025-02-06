2c91b26ff176be3ad9eceef46a6ea16b5d85f739
Ana Luna <ana.luna@bootcamp-upgrade.com>
2025-02-06 18:33:08 +0100

    Update app2_4.py

--- app2_4.py
+++ app2_4.py
@@ -457,79 +457,80 @@ if menu_option == "Prediction Model":
     
 
    #PRIMERO LA URL QUE CONDUCE A NUESTRO MODELO MAS LA CLAVE API QUE NOS PERMITE ACCEDER A EL
-    url = 'http://26639feb-ab23-4601-a19e-b8d6dcc1d173.eastus2.azurecontainer.io/score' # URL of the API
-    api_key = 'oDJGJJz8PRmZB0e2IN5bSPiCvYUX1KEx'
-
+url = 'http://11bb7995-16fc-4438-92c4-60f56e3d910d.eastus2.azurecontainer.io/score' # URL of the API
+api_key = '1SWzSyGfAVTb56Tf2gjlEB0ENaKCaCtO'
+ 
 #FUNCION DE PREDICCION
-    def get_prediction(data):
-        body = str.encode(json.dumps(data))
-        headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
-        req = urllib.request.Request(url, body, headers)
-
-        try:
-            response = urllib.request.urlopen(req)
-            result = response.read()
-            return json.loads(result)
-        except urllib.error.HTTPError as error:
-            print("The request failed with status code: " + str(error.code))
-            print(error.info())
-            print(json.loads(error.read()))
-            return None
-
-    #CONFIGURACION DE LA APP
-    st.title('Heart disease prediction')
-    st.write('Heart disease prediction app')
-
-
-    #DEFINIR LAS VARIABLES QUE NUESTRO MODELO CONOCE
-    
-    age = st.number_input('Age', min_value=1 , max_value=90, value=1)
-    sex = st.selectbox('Sex (1= Male, 0= Female)', ['1', '0'])
-    cp = st.selectbox('Chest pain (1= typical angina, 2= atypical angina, 3= non-anginal pain, 4= Asymptomatic)', ['1', '2', '3', '4'])
-    trestbps = st.number_input('Resting blood pressure', min_value=90, max_value=180, value=90)
-    chol = st.number_input('Cholesterol', min_value=150, max_value=450, value=150)
-    fbs = st.selectbox('Fasting blood sugar > 120 mg/dl (1 = True; 0 = False)', ['0', '1'])
-    restecg = st.selectbox('Resting electrocardiographic results (0= Normal, 1= ST-T wave abnormalities, 2= Left ventricular hypertrophy)', ['0', '1', '2'])
-    thalach = st.number_input('Maximum heart rate achieved ', min_value=90, max_value=200, value=90)
-    exang = st.selectbox('Exercise induced angina (1= The patient experienced angina, 0= The patient did not experience angina.)', ['0 ', '1 '])
-    oldpeak = st.number_input('ST depression induced by exercise relative to rest', min_value=0.0, max_value=6.2, value=0.1)
-    slope = st.selectbox('The slope of the peak exercise ST segment (1= Upsloping, 2= Flat, 3= Downsloping)', ['1', '2', '3'])
-    ca = st.selectbox('Number of major vessels (0-3) colored by flourosopy (0= No blockage, 1, 2, or 3= Indicates increasing degrees of obstruction)', ['0', '1', '2', '3'])
-    thal = st.selectbox('Thalassemia (3= Normal, 6= Fixed defect, 7= Reversible defect)', ['3', '6', '7'])
-
-    #DICCIONARIO DE DATOS
-    data = {
-        "Inputs": {
-            "input1": [
-                {
-                    'age': age,
-                    'sex': sex,
-                    'cp': cp,
-                    'trestbps': trestbps,
-                    'chol': chol,
-                    'fbs': fbs,
-                    'restecg': restecg,
-                    'thalach': thalach,
-                    'exang': exang,
-                    'oldpeak': oldpeak,
-                    'slope': slope,
-                    'ca': ca,
-                    'thal': thal,
-                }
-            ]
-        },
-        "GlobalParameters": {}
-    }
-
-    # Botón predict
-    if st.button('Chances of heart disease'):
-        result = get_prediction(data)
-
-        if result:
-            st.write('Chances of heart disease: ', result['Results']['WebServiceOutput0'][0]['predicted_heart_condition'])
-        else:
-            st.write('Something went wrong')
-
+def get_prediction(data):
+    body = str.encode(json.dumps(data))
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+    req = urllib.request.Request(url, body, headers)
+ 
+    try:
+        response = urllib.request.urlopen(req)
+        result = response.read()
+        return json.loads(result)
+    except urllib.error.HTTPError as error:
+        print("The request failed with status code: " + str(error.code))
+        print(error.info())
+        print(json.loads(error.read()))
+        return None
+ 
+#CONFIGURACION DE LA APP
+st.title('Heart disease prediction')
+st.write('This is a simple price prediction app')
+ 
+ 
+#DEFINIR LAS VARIABLES QUE NUESTRO MODELO CONOCE
+ 
+age = st.number_input('Age', min_value=1 , max_value=90, value=1)
+sex = st.selectbox('Sex', ['1', '0'])
+cp = st.selectbox('Chest pain', ['1', '2', '3', '4'])
+trestbps = st.number_input('Resting blood pressure', min_value=90, max_value=180, value=90)
+chol = st.number_input('Cholesterol', min_value=150, max_value=450, value=150)
+fbs = st.selectbox('Fasting blood sugar > 120 mg/dl', ['0', '1'])
+restecg = st.selectbox('Resting electrocardiographic results', ['0', '1', '2'])
+thalach = st.number_input('Maximum heart rate achieved', min_value=90, max_value=200, value=90)
+exang = st.selectbox('Exercise induced angina', ['0', '1'])
+oldpeak = st.number_input('ST depression induced by exercise relative to rest', min_value=0.0, max_value=6.2, value=0.1)
+slope = st.selectbox('The slope of the peak exercise ST segment', ['1', '2', '3'])
+ca = st.selectbox('Number of major vessels (0-3) colored by flourosopy', ['0', '1', '2', '3'])
+thal = st.selectbox('Thalassemia', ['3', '6', '7'])
+ 
+ 
+#DICCIONARIO DE DATOS
+data = {
+    "Inputs": {
+        "input1": [
+            {
+                'age': age,
+                'sex': sex,
+                'cp': cp,
+                'trestbps': trestbps,
+                'chol': chol,
+                'fbs': fbs,
+                'restecg': restecg,
+                'thalach': thalach,
+                'exang': exang,
+                'oldpeak': oldpeak,
+                'slope': slope,
+                'ca': ca,
+                'thal': thal,
+            }
+        ]
+    },
+    "GlobalParameters": {}
+}
+ 
+# Botón predict
+if st.button('Chances of heart disease'):
+    result = get_prediction(data)
+ 
+    if result:
+        st.write('Chances of heart disease: ', result['Results']['output1'][0]['predicted_heart_condition'])
+    else:
+        st.write('Something went wrong')
+   
  # Show prediction model dictionary
 if menu_option == "Prediction Model Dictionary":
     st.write("### Prediction Model Dictionary")
